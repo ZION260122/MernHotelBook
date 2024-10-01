eb2601bb49eb60ff6b17209f3bb2ebfbad700190
utkarsh <98471140+ZION260122@users.noreply.github.com>
2024-10-01 23:13:49 +0530

    I have fixed the bug

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -8,24 +8,33 @@ const FeaturedProperties = () => {
     <div className="fp">
       {loading ? (
         "Loading"
+      ) : error ? (
+        "An error occurred"
       ) : (
         <>
-          {data.map((item) => (
-            <div className="fpItem" key={item._id}>
-              <img
-                src={item.photos[0]}
-                alt=""
-                className="fpImg"
-              />
-              <span className="fpName">{item.name}</span>
-              <span className="fpCity">{item.city}</span>
-              <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
-              {item.rating && <div className="fpRating">
-                <button>{item.rating}</button>
-                <span>Excellent</span>
-              </div>}
-            </div>
-          ))}
+          {/* Ensure data is an array before mapping */}
+          {Array.isArray(data) && data.length > 0 ? (
+            data.map((item) => (
+              <div className="fpItem" key={item._id}>
+                <img
+                  src={item.photos[0]}
+                  alt=""
+                  className="fpImg"
+                />
+                <span className="fpName">{item.name}</span>
+                <span className="fpCity">{item.city}</span>
+                <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
+                {item.rating && (
+                  <div className="fpRating">
+                    <button>{item.rating}</button>
+                    <span>Excellent</span>
+                  </div>
+                )}
+              </div>
+            ))
+          ) : (
+            <p>No featured properties available</p>
+          )}
         </>
       )}
     </div>
